d38c7de1d54650103cdbd52bd77f4b04b36232fb
Umar Thariq <umar.thariq@gdn-commerce.com>
2018-06-07 18:01:31 +0530

    card style changes

---
+++ scripts/controllers/mainController.js
@@ -0,0 +1,53 @@
+angular.module("tmt")
+.controller("mainCtrl", function($scope){
+    $scope.movieObject = [{
+        movieName: 'Anwar',
+        rating: 3,
+        actor: 'Siddharth Koirala',
+        actress: 'Nauheed Cyrusi.',
+        certificate: 'U/A'
+    },{
+        movieName: 'Anwar',
+        rating: 2,
+        actor: 'Siddharth Koirala',
+        actress: 'Nauheed Cyrusi.',
+        certificate: 'U/A'
+    },{
+        movieName: 'Anwar',
+        rating: 5,
+        actor: 'Siddharth Koirala',
+        actress: 'Nauheed Cyrusi.',
+        certificate: 'U/A'
+    },{
+        movieName: 'Anwar',
+        rating: 1,
+        actor: 'Siddharth Koirala',
+        actress: 'Nauheed Cyrusi.',
+        certificate: 'U/A'
+    },{
+        movieName: 'Anwar',
+        rating: 4,
+        actor: 'Siddharth Koirala',
+        actress: 'Nauheed Cyrusi.',
+        certificate: 'U/A'
+    },{
+        movieName: 'Anwar',
+        rating: 4,
+        actor: 'Siddharth Koirala',
+        actress: 'Nauheed Cyrusi.',
+        certificate: 'U/A'
+    },{
+        movieName: 'Anwar',
+        rating: 2,
+        actor: 'Siddharth Koirala',
+        actress: 'Nauheed Cyrusi.',
+        certificate: 'U/A'
+    },{
+        movieName: 'Anwar',
+        rating: 5,
+        actor: 'Siddharth Koirala',
+        actress: 'Nauheed Cyrusi.',
+        certificate: 'U/A'
+    }];
+    $scope.movie = "something for movie";
+});
--- scripts/directives/movieCard/index.js
+++ scripts/directives/movieCard/index.js
@@ -1,6 +1,21 @@
 angular.module("tmt")
 .directive("movieCard", function(){
     return{
+        restrict: 'E',
+        scope:{
+            movieInfo:'='
+        },
+        link: function(scope, elem ,attr){
+            var movieRating = [{mark: 0}, {mark:0}, {mark: 0}, {mark: 0}, {mark: 0}];
+            var rating = scope.movieInfo.rating;
+            scope.movieRating = movieRating.map(function(ele){
+                if(rating > 0){
+                    ele.mark = 1;
+                }
+                rating = rating-1;
+                return ele;
+            });
+        },
         templateUrl: "./directives/movieCard/index.htm"
     }
 });
