db068a9ed98275e3b17ba1f0d5d0f468920a63cb
Umar Thariq <umar.thariq@gdn-commerce.com>
2018-06-20 17:30:14 +0530

    node port changes

--- app.js
+++ app.js
@@ -89,4 +89,6 @@ app.delete('/deleteMovie', (req, res)=>{
   });
 });
 
-app.listen(5000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
